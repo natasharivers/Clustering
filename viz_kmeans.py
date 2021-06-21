a6d6cbc307d080f03b008fe52af08e066ef8218e
Natasha Rivers <natasha.tanya.rivers@gmail.com>
2021-06-21 12:35:34 -0500

    create visualization function for mall customers

--- viz_kmeans.py
+++ viz_kmeans.py
@@ -14,6 +14,9 @@ def make_blob():
     plt.scatter(X[:, 0], X[:, 1], s=30, color = 'gray')
     return plt.show()
 
+########################## Visualize Iris Clusters ##########################
+
+
 def viz_iris(iris, kmeans):
     
     centroids = np.array(iris.groupby('cluster')['petal_length', 'sepal_length'].mean())
@@ -52,5 +55,52 @@ def viz_iris(iris, kmeans):
     
     
     iris.drop(columns = ['cen_x', 'cen_y', 'c'], inplace = True)
+    plt.tight_layout()
+    plt.show()
+
+########################## Visualize Mall Customers Clusters ##########################
+
+def viz_mall_customers(mall_customers, kmeans):
+    '''
+    This function creates visualizations of actual spending score vs predicted spending score
+    with clusters and centroid from mall_customers data on codeup sql database
+    '''
+    
+    centroids = np.array(mall_customers.groupby('cluster')['age', 'annual_income'].mean())
+    cen_x = [i[0] for i in centroids]
+    cen_y = [i[1] for i in centroids]
+    # cen_x = [i[0] for i in kmeans.cluster_centers_]
+    # cen_y = [i[1] for i in kmeans.cluster_centers_]
+    mall_customers['cen_x'] = mall_customers.cluster.map({0:cen_x[0], 1:cen_x[1], 2:cen_x[2]})
+    mall_customers['cen_y'] = mall_customers.cluster.map({0:cen_y[0], 1:cen_y[1], 2:cen_y[2]})
+
+    colors = ['#DF2020','#2095DF', '#81DF20' ]
+    mall_customers['c'] = mall_customers.cluster.map({0:colors[0], 1:colors[1], 2:colors[2]})
+    #plot scatter chart for Actual species and those predicted by K - Means
+
+    #specify custom palette for sns scatterplot
+    colors1 = ['#2095DF','#81DF20' ,'#DF2020']
+    customPalette = sns.set_palette(sns.color_palette(colors1))
+
+    #plot the scatterplots
+
+    #Define figure (num of rows, columns and size)
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10,10))
+
+    # plot ax1 
+    ax1 = plt.subplot(2,1,1) 
+    sns.scatterplot(data = mall_customers, x = 'age', y = 'annual_income', ax = ax1, hue = 'spending_score', palette=customPalette)
+    plt.title('Actual Spending Score')
+
+    #plot ax2
+    ax2 = plt.subplot(2,1,2) 
+    ax2.scatter(mall_customers.age, mall_customers.annual_income, c=mall_customers.c, alpha = 0.6, s=10)
+    ax2.set(xlabel = 'age of customer', ylabel = 'annual income', title = 'K - Means')
+
+    # plot centroids on  ax2
+    ax2.scatter(cen_x, cen_y, marker='X', c=colors, s=200)
+    
+    
+    mall_customers.drop(columns = ['cen_x', 'cen_y', 'c'], inplace = True)
     plt.tight_layout()
     plt.show()
